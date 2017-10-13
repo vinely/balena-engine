108d182ea64aedabab5e511e60ede053a3f27490
craig-mulligan <craig@resin.io>
2017-10-13 07:49:09 +0100

    Deployed by landr 🏠

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ esac
 
 url="https://github.com/resin-os/balena/releases/download/${tag}/balena-${tag}-${arch}.tar.gz"
 
-curl -sL "$url" | sudo tar xzv -C /usr/local/bin --strip-components=1
+# curl -sL "$url" | sudo tar xzv -C /usr/local/bin --strip-components=1
 
 cat <<EOF
 
